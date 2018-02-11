18f9bbfb0da084840af25e7ced20a7a43b03ced3
Yogashri Pradhan <ypradhan.pge@utexas.edu>
2018-02-11 17:14:18 -0600

    correcting server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connect = require ("connect");
 //const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
