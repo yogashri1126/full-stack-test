19ab25c6a3b7b44f58554398bdea3889c6b5b69e
Yogashri Pradhan <ypradhan.pge@utexas.edu>
2018-02-11 23:24:20 -0600

    added react component to render table data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Map from './Map2';
+import {data} from './Table'
 
 // App component
 class App extends Component {
   render() {
     return <Map />;
+    return data;
   }
 }
 
